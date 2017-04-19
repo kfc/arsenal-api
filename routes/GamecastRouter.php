ed9aeff22efb16897fc7119af0c2d004e6c71174
Yury Bartashevich <yury.bartashevich@neto.com.au>
2017-04-20 09:04:43 +1000

    Restore match-squad route

--- routes/GamecastRouter.php
+++ routes/GamecastRouter.php
@@ -38,6 +38,14 @@ class GamecastRouter extends ApiRouter {
 			$app->response->body($data);
 		});
 
+		$app->get('/match-squads/:match_nid', function ($match_nid) use ($app, $cache_key, $api) {
+			$data = $app->cache->get($cache_key);
+			if($data == null) {
+				$data = $api->getMatchSquads($match_nid);
+				$app->cache->set($cache_key, $data, 0);
+			}
+			$app->response->body($data);
+		});
 
 		$app->get('/tournament-table', function () use ($app, $cache_key, $api) {
 			$data = $app->cache->get($cache_key);
